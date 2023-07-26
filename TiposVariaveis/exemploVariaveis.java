120e52e386b1933258c035c4b3baf5af2682c0df
Jonathan <cavalcantib301@gmail.com>
2023-07-26 17:45:54 -0300

    Exemplos de variáveis e seus tipos

--- TiposVariaveis/exemploVariaveis.java
+++ TiposVariaveis/exemploVariaveis.java
@@ -2,6 +2,10 @@ package TiposVariaveis;
 
 public class exemploVariaveis {
     public static void main(String[] args) {
-
+        final double VALOR_DE_PI = 3.14;
+        double salarioMinimo = 2500;
+        short numeroNormal = 1;
+        int numeroCurto;
+        System.out.println(VALOR_DE_PI);
     }
 }
